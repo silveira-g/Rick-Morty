c812a144256856b7a6074a01d0e905b457772a16
Guilherme Silveira <silveira.alves.guilherme@gmail.com>
2021-10-07 21:19:00 -0300

    Criação do card de personagem

--- src/Pages/Personagens.vue
+++ src/Pages/Personagens.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="home">
-    <h1>Lista de Personagens crua</h1>
-    <span>{{ listPersonagens }}</span>
+    <CardPersonagem />
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
-
+import CardPersonagem from "../components/CardPersonagem.vue";
 export default {
+  components: { CardPersonagem },
   name: "Personagens",
   created() {
     this.getPersonagens();
---
+++ src/components/CardPersonagem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-row>
+      <b-card
+        v-for="personagem in listPersonagens"
+        :key="personagem.index"
+        :title="personagem.name"
+        :img-src="personagem.image"
+        img-alt="Imag"
+        style="max-width: 20rem"
+        class="card-style"
+      >
+        <b-card-text> </b-card-text>
+      </b-card>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "Personagem",
+
+  computed: {
+    ...mapGetters(["listPersonagens"]),
+  },
+};
+</script>
+
+<style>
+</style>
